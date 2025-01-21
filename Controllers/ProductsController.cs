859fac948d74b86001cbfbb3b130c125ed686b18
ruanoli <ruanoliveira.pro@gmail.com>
2025-01-21 02:29:04 -0300

    create API

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Services.Models;
+
+namespace ProductAPI.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet("GetAllProduct")]
+        public IActionResult GetAll()
+        {
+            //TODO
+
+            return Ok();
+        }
+
+        [HttpPost("RegisterProduct")]
+        public IActionResult Register(ProductPostRequest model)
+        {
+            //TODO
+
+            return Ok();
+        }
+
+        [HttpPut("UpdateProduct")]
+        public IActionResult Update(ProductPostRequest model)
+        {
+            //TODO
+
+            return Ok();
+        }
+
+        [HttpDelete("DeleteProduct")]
+        public IActionResult Delete(ProductPostRequest model)
+        {
+            //TODO
+
+            return Ok();
+        }
+    }
+}
